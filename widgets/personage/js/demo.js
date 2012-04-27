02c8bbec98bb2a6028ae7c48f103138b6a3c1d11
Andrey <Andrey@.(none)>
2012-04-27 12:10:51 +0200

    added dbpedia loader

--- widgets/personage/js/demo.js
+++ widgets/personage/js/demo.js
@@ -1,8 +1,5 @@
 $ (function ()  {
-  var myVIE = window.myVIE = new VIE();
-  myVIE.use(new myVIE.DBPediaService);
-  myVIE.load();
-  var text = ("<h1> The Beatles </h1>" +
+	var text = ("<h1> The Beatles </h1>" +
               "The Beatles were an English rock band formed in Liverpool in 1960," +
               "and one of the most commercially successful and critically acclaimed acts in the history of popular music." +
               "The group's best-known lineup consisted of John Lennon (rhythm guitar, vocals)," +
@@ -14,10 +11,23 @@ $ (function ()  {
               "as their songwriting grew in sophistication by the late 1960s," +
               "they came to be perceived by many fans and cultural observers as" +
               "an embodiment of the ideals shared by the era's sociocultural revolutions.");
-  replaceText(text);
-  urlImage = 'http://upload.wikimedia.org/wikipedia/commons/thumb/d/df/The_Fabs.JPG/600px-The_Fabs.JPG';
-  replaceImage (urlImage);
-  $('#sample_img').viePersonage({FACE_API_KEY: "16fc0307893bfc78a015c141c6e584bd"});
+	replaceText(text);
+	urlImage = 'http://upload.wikimedia.org/wikipedia/commons/thumb/d/df/The_Fabs.JPG/600px-The_Fabs.JPG';
+	replaceImage (urlImage);
+	//initialize face tagger 
+	$('#sample_img').viePersonage({FACE_API_KEY: "16fc0307893bfc78a015c141c6e584bd"});
+  
+	//Instantiate VIE and load entities
+	var entities = 'http://dbpedia.org/page/John_Lennon';
+	var myVIE = window.myVIE = new VIE();
+	myVIE.use(new myVIE.DBPediaService());
+	myVIE
+	.load({entity: entities})
+	.using('dbpedia')
+	.execute()
+	.done(function(){
+		alert('done!');
+	});
 });
 
 function replaceText(text) {
