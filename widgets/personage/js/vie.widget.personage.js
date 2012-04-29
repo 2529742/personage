61154f889cab47c7e9f52555a37a5c73b076b2d3
Andrey <Andrey@.(none)>
2012-04-29 15:09:10 +0200

    added image annotation method

--- widgets/personage/js/vie.widget.personage.js
+++ widgets/personage/js/vie.widget.personage.js
@@ -6,21 +6,30 @@
        },
        
        _init: function () {
-            this._tagFace();
+			var self = this;
+			var img_id = $(self.element).attr('id');
+			this.tagFace(img_id,this.annotate_faces);
        },
 
-       _tagFace: function() {
-            var self = this;
-            FaceTagger.load("#" + $(self.element).attr('id'), {
+       tagFace: function(img_id, callback) {
+            FaceTagger.load('#' + img_id, {
                 click_add_tag: false,
                 resizable: true,
                 facebook: true,
                 fade: true,
                 tags_list: true,
                 add_tag_button: true,
-                demo_mode: true
+                demo_mode: true,
+				success: function(img, response){
+					var tags = response? (response.photos? response.photos[0].tags: undefined): undefined;
+					callback(tags);
+				}
            });
         },
+		
+		annotate_faces: function(tags) {
+		
+		},
         
         options: {
            FACE_API_KEY: undefined
