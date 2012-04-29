fc5dd41beb40cf3ef2bf272d3264241510b6cf8b
cholpon <cholpon7@gmail.com>
2012-04-29 14:03:03 +0200

    added annotations

--- widgets/personage/js/demo.js
+++ widgets/personage/js/demo.js
@@ -1,11 +1,13 @@
 $ (function ()  {
-	var text = ("<h1> The Beatles </h1>" +
-              "The Beatles were an English rock band formed in Liverpool in 1960," +
+
+  var text = ("<h1> The Beatles</h1>" +
+              "<span about= \"http://dbpedia.org/resource/The_Beatles\" typeof= \"Organisation\" > The Beatles </span> were an English rock band formed in Liverpool in 1960," +
               "and one of the most commercially successful and critically acclaimed acts in the history of popular music." +
-              "The group's best-known lineup consisted of John Lennon (rhythm guitar, vocals)," +
-              "Paul McCartney (bass guitar, vocals), George Harrison (lead guitar, vocals)" +
-              "and Ringo Starr (drums, vocals). Rooted in skiffle and 1950s rock and roll," +
-              "the group later worked in many genres ranging from pop ballads to psychedelic rock," +
+              "The group's best-known lineup consisted of <span about=\"http://dbpedia.org/resource/John_Lennon\" typeof= \"Person\"> John Lennon </span> (rhythm guitar, vocals)," +
+              "<span about=\"http://dbpedia.org/resource/Paul_McCartney\" typeof=\"Person\"> Paul McCartney </span> (bass guitar, vocals)," +
+              "<span about=\"http://dbpedia.org/resource/George_Harrison\" typeof=\"Person\"> George Harrison </span>(lead guitar, vocals)" +
+              "and <span about=\"http://dbpedia.org/resource/Ringo_Starr\" typeof=\"Person\"> Ringo Starr </span>(drums, vocals). Rooted in skiffle and 1950s rock and roll," +
+              "the group later worked in many genres ranging from pop ballad s to psychedelic rock," +
               "often incorporating classical and other elements in innovative ways." +
               "Their enormous popularity first emerged as \"Beatlemania\";" +
               "as their songwriting grew in sophistication by the late 1960s," +
