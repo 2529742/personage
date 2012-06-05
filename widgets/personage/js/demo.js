ac234237baf997681c3e422e4f24b52d24a176be
Andrey <Andrey@.(none)>
2012-06-05 19:18:39 +0200

    changed the appearance

--- widgets/personage/js/demo.js
+++ widgets/personage/js/demo.js
@@ -1,7 +1,7 @@
 $ (function ()  {
 
-  var text = ("<h1> The Beatles</h1>" +
-              "The Beatles were an English rock band formed in Liverpool in 1960," +
+  var text = ("<h2> The Beatles</h2>" +
+              "<section>The Beatles were an English rock band formed in Liverpool in 1960," +
               " and one of the most commercially successful and critically acclaimed acts in the history of popular music." +
               " The group's best-known lineup consisted of <span about=\"http://dbpedia.org/resource/John_Lennon\" typeof= \"Person\"> John Lennon </span> (rhythm guitar, vocals)," +
               " <span about=\"http://dbpedia.org/resource/Paul_McCartney\" typeof=\"Person\"> Paul McCartney </span> (bass guitar, vocals)," +
@@ -12,7 +12,7 @@ $ (function ()  {
               " Their enormous popularity first emerged as \"Beatlemania\";" +
               " as their songwriting grew in sophistication by the late 1960s," +
               " they came to be perceived by many fans and cultural observers as" +
-              " an embodiment of the ideals shared by the era's sociocultural revolutions.");
+              " an embodiment of the ideals shared by the era's sociocultural revolutions.</section><br/>");
     replaceText(text);
     urlImage = 'http://upload.wikimedia.org/wikipedia/commons/thumb/d/df/The_Fabs.JPG/600px-The_Fabs.JPG';
     replaceImage (urlImage);
@@ -24,7 +24,7 @@ $ (function ()  {
         'http://dbpedia.org/resource/Ringo_Starr',
         'http://dbpedia.org/resource/Paul_McCartney'
     ];
-	var content = $('#content');
+	var content = $('article');
     var myVIE = window.myVIE = new VIE();
     myVIE.use(new myVIE.StanbolService, 'stanbol');
     myVIE
@@ -36,15 +36,23 @@ $ (function ()  {
     .done(function(){
     });
     //activate draggables
-    var persons = $('#content').children('[typeof="Person"]');
+    var persons = $('article').find('[typeof="Person"]');
     $(persons).each(function(){
         entityDrag(this);
     });
-    
+	
+	persons
+	.attr("title", "You can drag the entity and drop it on the image tag to create a semantic relation between the Person and it's image annotation.")
+	.tipTip();
+
+	$('#start')
+	.attr("title", "Press the button to start the face detection an the image annotation.")
+	.tipTip();
   //highlight photo on hover
     $(persons).each(function() {
         $(this).hover(
             function()  {
+				$(this).addClass('hover');
                 var about = $(this).attr('about');
                 var person_entity= myVIE.entities.get(about);
                 if (person_entity) {
@@ -55,6 +63,7 @@ $ (function ()  {
                 }
             }, 
             function()  { 
+				$(this).removeClass('hover');
                 var about = $(this).attr('about');
                 var person_entity= myVIE.entities.get(about);
                 if (person_entity) {
@@ -95,7 +104,7 @@ function startAnnotation(){
     });
 }
   function replaceText(text) {
-    $("#content").html(text);
+    $("article").html(text);
   };
 
   function replaceImage(urlImage) {
